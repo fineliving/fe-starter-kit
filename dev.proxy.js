64cc5c6f61c8c63750d515d249b9b1f6f6bbf74c
fineliving <liyang_qifengle@126.com>
2020-11-13 00:46:56 +0800

    第一次build提交

--- dev.proxy.js
+++ dev.proxy.js
@@ -1,9 +1,9 @@
 module.exports = {
-  '/api_plan': {
-    target: 'https://fineliving.github.io/',
+  "/api_plan": {
+    target: "https://fineliving.github.io/",
     // changeOrigin: true,
     pathRewrite: {
-      '^/api_plan': '/',
-    },
-  },
-}
+      "^/api_plan": "/"
+    }
+  }
+};
